7f34b07c5db75d59c2cd631bf78ccc33dd5dff70
Alex Brilevski <brilevski.alex@gmail.com>
2026-02-17 20:36:20 +0300

    Logic of opening and closing accordion items was implemented

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,19 @@ function App() {
         <h2>Why work with us?</h2>
         <Accordion className={'accordion'}>
           <AccordionItem
+            id={'experience'}
             className={'accordion-item'}
             title={'We got 20 years of experience'}
           >
             <article>
-              <p>You can&apost;t go wrong with us.</p>
+              <p>You can't go wrong with us.</p>
               <p>
                 We are in the business of planning highly individualized vacation trips for more than 20 years.
               </p>
             </article>
           </AccordionItem>
           <AccordionItem
+            id={'local-guides'}
             className={'accordion-item'}
             title={'We are working with local guides'}
           >
--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,7 +1,39 @@
+import { createContext, useContext, useState } from 'react';
+
+const AccordionContex = createContext();
+
+export function useAccordionContex() {
+  const ctx = useContext(AccordionContex);
+
+  if (!ctx) {
+    throw new Error('Accordion-related components must be wrapped by <Accordion>');
+  }
+
+  return ctx;
+}
+
 export default function Accordion({ className, children }) {
+  const [openItemId, setOpenItemId] = useState();
+
+  function openItem(id) {
+    setOpenItemId(id);
+  }
+
+  function closeItem() {
+    setOpenItemId(null);
+  }
+
+  const contexValue = {
+    openItemId,
+    openItem,
+    closeItem,
+  };
+
   return (
-    <ul className={className}>
-      {children}
-    </ul>
+    <AccordionContex.Provider value={contexValue}>
+      <ul className={className}>
+        {children}
+      </ul>
+    </AccordionContex.Provider>
   );
 }
