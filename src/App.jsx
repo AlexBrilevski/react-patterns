e82804170ed188939df99158cd5326e5145376fa
Alex Brilevski <brilevski.alex@gmail.com>
2026-02-20 21:51:40 +0300

    Basic Searchable List component was added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,52 @@
+import SearchableList from "./components/SearchableList/SearchableList";
 import Accordion from "./components/Accordion/Accordion";
+
+import savannaImg from './assets/african-savanna.jpg';
+import amazonImg from './assets/amazon-river.jpg';
+import caribbeanImg from './assets/caribbean-beach.jpg';
+import desertImg from './assets/desert-dunes.jpg';
+import forestImg from './assets/forest-waterfall.jpg';
+
+const PLACES = [
+  {
+    id: 'african-savanna',
+    image: savannaImg,
+    title: 'African Savanna',
+    description: 'Experience the beauty of nature.',
+  },
+  {
+    id: 'amazon-river',
+    image: amazonImg,
+    title: 'Amazon River',
+    description: 'Get to know the largest river in the world.',
+  },
+  {
+    id: 'caribbean-beach',
+    image: caribbeanImg,
+    title: 'Caribbean Beach',
+    description: 'Enjoy the sun and the beach.',
+  },
+  {
+    id: 'desert-dunes',
+    image: desertImg,
+    title: 'Desert Dunes',
+    description: 'Discover the desert life.',
+  },
+  {
+    id: 'forest-waterfall',
+    image: forestImg,
+    title: 'Forest Waterfall',
+    description: 'Listen to the sound of the water.',
+  },
+];
 
 function App() {
   return (
     <main>
+      <section>
+        <SearchableList items={PLACES}/>
+        <SearchableList items={['item-1', 'item-2']}/>
+      </section>
       <section>
         <h2>Why work with us?</h2>
         <Accordion className={'accordion'}>
