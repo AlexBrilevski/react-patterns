178cc1cf46a56d582d9303067704e8da538e8d32
Alex Brilevski <brilevski.alex@gmail.com>
2026-02-20 22:06:59 +0300

    Basic search logic was added to the Searchable List component

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 export default function SearchableList({ items }) {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  function handleChange(event) {
+    setSearchTerm(event.target.value);
+  }
+
+  const searchResults = items.filter(item =>
+    JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="searchable-list">
-      <input type="search" placeholder="Search" />
+      <input onChange={handleChange} type="search" placeholder="Search" />
       <ul>
-        {items.map((item, index) => (
+        {searchResults.map((item, index) => (
           <li key={index}>{item.toString()}</li>
         ))}
       </ul>
